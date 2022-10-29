fe68fa7ea232565913082814f30fcc65f9f63714
BurntJannings <82676805+BurntJannings@users.noreply.github.com>
2022-10-29 05:35:15 -0600

    Language Added

--- config.lua
+++ config.lua
@@ -10,3 +10,16 @@ Config.doctors = {
 
     job = { "doctor", "police" }
 }
+
+
+Config.Language = {
+
+calldoctor = "Doctor Called",
+notdead = "You are not dead",
+inwater = "Doctor Fell in Water, Call for another",
+missground= "Doctor couldn't find the ground, Call for another",
+doctordied = "Doctor died, Call for another",
+revived = "You have been revived for $",
+notenough = "You do not have $",
+doctoractive = "A Doctor is available, /alert instead"
+}
